71d923807e1038d4eebb376b12c1b7d5a0e8dcd7
Robert Evans <rlevans91@gmail.com>
2014-03-11 17:45:45 +0000

    Detecting the orange region at the base of the box

--- experiment_histogram.m
+++ experiment_histogram.m
@@ -26,6 +26,17 @@ depthMask = ~isnan(frame(:,:,1)) & ~isnan(frame(:,:,2)) & ~isnan(frame(:,:,3));
 % Combine depth and chromaticity masks
 rgbdMask = rgbChMask & depthMask;
 
+% Find two large objects in the mask (the orange parts of the box)
+stats = regionprops(rgbdMask,'Area','PixelIdxList','Centroid');
+[sortedAreas,sortedAreaIndexes] = sort([stats.Area], 'descend');
+
+% Find the region lowest in the image (the bottom of the box).
+if stats(sortedAreaIndexes(1)).Centroid(2) >= stats(sortedAreaIndexes(2)).Centroid(2)
+    box_base = sortedAreaIndexes(1);
+else
+    box_base = sortedAreaIndexes(2);
+end
+
 % Apply combined thresholds to the image
 chr_masked_image = repmat(rgbdMask, 1, 1, 3) .* frame_ch(:,:,1:3);
 
@@ -35,6 +46,10 @@ imshow(frame_ch(:,:,1:3)); % chromaticity image
 figure(3)
 imshow(frame(:,:,4:6)/255); % rgb image
 show_depth_image(frame, 4); % depth image
+figure(5)
+im_box_bottom = zeros( size(rgbdMask) );
+im_box_bottom( stats(box_base).PixelIdxList ) = 1;
+imshow(im_box_bottom) % detected base of box (orange region)
 
 % Display isolated regions
 figure(1)
