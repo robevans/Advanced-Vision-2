40defd25b3bfbc5c76084ff399e74a7e8e7f3abd
Tadas Subonis <tadas.subonis@gmail.com>
2014-03-15 18:38:16 +0000

    Some finetuning to the function

--- plot_plane_colors.m
+++ plot_plane_colors.m
@@ -13,7 +13,8 @@ x_max_val = range(2);
 y_min_val = range(3);
 y_max_val = range(4);
 
-step = 0.01;
+%step = 0.003;
+step = 0.005;
 
 depth_points = points(:, 1:3);
 
@@ -35,6 +36,9 @@ Assignments = zeros(length(points), 1);
 
 %for each point calculate distance and assign for real
 %point closest sampled point
+
+%possible to use pdist2 but we get out of memory error
+disp('Calculating distances');
 for i = 1:length(depth_points),
     cm = depth_points(i, :);
     dist = abs(bsxfun(@minus, Y, cm));
@@ -48,6 +52,7 @@ colors = zeros(size(Y));
 
 %for each sampled point calculated averaged color from
 %assigned real points
+disp('Calculating colors');
 for i = 1:length(Y),
     entry_colors = points(Assignments == i, 4:6);
     color_mean = mean(entry_colors);
@@ -63,7 +68,7 @@ C = reshape(colors, [size(xx, 1) size(yy, 2) 3]);
 
 % plot the surface
 figure(figure_handle)
-surf(xx, yy, zz, C)
+surf(xx, yy, zz, C,'EdgeColor','none','LineStyle','none')
 
 xlabel('x')
 ylabel('y')
